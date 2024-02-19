df2a1d37bb35e1d8cfb41f48b44147baa292309d
finiral <finiral@yahoo.fr>
2024-02-19 12:10:03 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Wpf.Ui.Controls;
+
+namespace WpfApp1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : FluentWindow
+{
+    public MainWindow()
+    {
+        this.ExtendsContentIntoTitleBar = true;
+        this.WindowBackdropType=WindowBackdropType.Tabbed;
+        InitializeComponent();
+    }
+
+    private void ToggleSwitch_Checked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        Debug.WriteLine("Activated !");
+        if (sender is ToggleSwitch toggleSwitch)
+        {
+            toggleSwitch.Content = "Stop Cleaning";
+        }
+    }
+
+    private void ToggleSwitch_Unchecked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        Debug.WriteLine("Unactivated !");
+        if (sender is ToggleSwitch toggleSwitch)
+        {
+            toggleSwitch.Content = "Start Cleaning";
+        }
+
+    }
+}
