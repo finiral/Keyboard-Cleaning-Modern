c6014a8599fdedc7bf30fd5229ccc73d87ac8c47
finiral <finiral@yahoo.fr>
2024-02-19 12:56:29 +0300

    bar left

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Automation;
+using System.Windows.Controls;
 using Wpf.Ui.Controls;
 
 namespace WpfApp1;
@@ -7,11 +10,26 @@ namespace WpfApp1;
 /// </summary>
 public partial class MainWindow : FluentWindow
 {
+    Boolean refreshStats = false;
+    PerformanceCounter cpuCounter;
+    PerformanceCounter ramCounter;
+    System.Threading.Timer timer;
     public MainWindow()
     {
-        this.ExtendsContentIntoTitleBar = true;
-        this.WindowBackdropType=WindowBackdropType.Tabbed;
         InitializeComponent();
+        this.refreshStats = true;
+        this.ExtendsContentIntoTitleBar = true;
+        this.WindowBackdropType = WindowBackdropType.Tabbed;
+        this.Closing += form_close;
+        this.Loaded += AppUi_Load;
+    }
+
+    private void AppUi_Load(object sender, EventArgs e)
+    {
+        cpuCounter = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total");
+        ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        // Initialize timer to update stats every second
+        timer = new System.Threading.Timer(UpdateStats, null, 0, 1000);
     }
 
     private void ToggleSwitch_Checked(object sender, System.Windows.RoutedEventArgs e)
@@ -19,6 +37,7 @@ public partial class MainWindow : FluentWindow
         Debug.WriteLine("Activated !");
         if (sender is ToggleSwitch toggleSwitch)
         {
+            KeyboardHook.SetHook();
             toggleSwitch.Content = "Stop Cleaning";
         }
     }
@@ -28,8 +47,50 @@ public partial class MainWindow : FluentWindow
         Debug.WriteLine("Unactivated !");
         if (sender is ToggleSwitch toggleSwitch)
         {
+            KeyboardHook.Unhook();
             toggleSwitch.Content = "Start Cleaning";
         }
 
+    }
+    private void form_close(object sender, CancelEventArgs e)
+    {
+        KeyboardHook.Unhook();
+        this.refreshStats = false;
+        Debug.WriteLine("closed");
+    }
+    public string getCurrentCpuUsage()
+    {
+        return (int)cpuCounter.NextValue() + "%";
+    }
+
+    public string getAvailableRAM()
+    {
+        return ramCounter.NextValue() + "MB";
+    }
+
+    private void UpdateStats(object state)
+    {
+        if (!this.refreshStats)
+        {
+            // Stop the timer if refreshStats is false
+            timer.Dispose();
+            return;
+        }
+
+        else
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                Debug.WriteLine("cpu " + getCurrentCpuUsage());
+                if (this.FindName("cpu") is Label cpu)
+                {
+                    cpu.Content = getCurrentCpuUsage();
+                }
+                if (this.FindName("ram") is Label ram)
+                {
+                    ram.Content = getAvailableRAM();
+                }
+            }));
+        }
     }
 }
